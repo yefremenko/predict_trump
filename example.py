918887ea92d1baf61dac89c3637bcb7f3be4a0a6
YevheniiaYefremenko <yevheniia.yefremenko@gmail.com>
2020-03-09 14:09:58 +0100

    add title to conf matrix plot

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from TrainedModel import TrainedModel
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 dataset = pd.read_csv("train_df.csv").fillna("none")
 dataset_A, dataset_B = train_test_split(dataset, test_size=0.2, shuffle=False)
@@ -9,5 +10,10 @@ trained_model_A = TrainedModel(dataset_A)
 performance = trained_model_A.evaluate_performance(dataset_B)
 confusion_matrix = trained_model_A.get_confusion_matrix(dataset_B)
 
-# trained_model_A.plot_confusion_matrix(dataset_B)
-# trained_model_A.predict_one("travel", "travel", "event")
+trained_model_A.plot_confusion_matrix(dataset_A)
+plt.title("Training")
+plt.show()
+trained_model_A.plot_confusion_matrix(dataset_B)
+plt.title("Validation")
+plt.show()
+trained_model_A.predict_one("travel", "travel", "events")
